ca393613394b9ad8406872517988a4d4264f6dee
Teresa-perezpozo <t.perezdearenaza@alumnos.upm.es>
2025-12-17 17:34:07 +0100

    Initial commit

---
+++ src/main/java/es/upm/dit/aled/lab7/Alta.java
@@ -0,0 +1,61 @@
+package es.upm.dit.aled.lab7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+
+
+/**
+ * Servlet implementation class Alta
+ */
+@WebServlet("/alta")
+public class Alta extends HttpServlet {
+
+    @Override
+    public void init() {
+    	if(getServletContext().getAttribute("repo") == null )
+    		getServletContext().setAttribute("repo", new PacienteRepository(getServletContext()));
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    InputStream file = getServletContext().getResourceAsStream("/alta.html");
+		InputStreamReader reader1 = new InputStreamReader(file);
+		BufferedReader html = new BufferedReader(reader1);
+
+		String pagina = "", linea;
+		while((linea = html.readLine()) != null)
+			pagina += linea;
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println(pagina);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+		String apellido = request.getParameter("apellido");
+		String dni = request.getParameter("dni");
+		
+		Paciente p = new Paciente()
+	
+	
+	
+	
+	
+	
+	}
+}
